d4a5ffda8a1d148ca7eec3c26dfa14231619ab60
Jared Dominguez <jdomingu@users.noreply.github.com>
2019-03-04 15:47:26 -0800

    Add script to update repo listing, update repo listing  (#8)
    * Feature Extensions API in carousel
    
    * Add missing utf-8 charset to index.html
    
    * Add script to update repo listing, update repo listing

---
+++ update_repos.js
@@ -0,0 +1,71 @@
+/* Update the repositories listed on the homepage.
+ *
+ * This script gets repositories in the tableau organization from the GitHub API
+ * and filters the response down to a subset of fields.
+ * Note: We can't load the API response directly because we hit API rate limits.
+ */
+const fs = require("fs");
+const https = require("https");
+
+const repoFile = "js/github_repos.json";
+const requestOpts = {
+  hostname: "api.github.com",
+  path: "/orgs/tableau/repos?per_page=100",
+  headers: {
+    "User-Agent": "request"
+  }
+};
+
+function filterJson(reposJson) {
+  let repos = [];
+  const fieldsToKeep = [
+    "name",
+    "html_url",
+    "id",
+    "description",
+    "stargazers_count",
+    "forks_count",
+    "language"
+  ];
+
+  JSON.parse(reposJson).forEach(function(repo) {
+    let repoJsonSubset = {};
+
+    for (var field in repo) {
+      if (fieldsToKeep.indexOf(field) !== -1) {
+        repoJsonSubset[field] = repo[field];
+      }
+    }
+
+    repos.push(repoJsonSubset);
+  });
+
+  return repos;
+}
+
+function writeJsonToFile(outStr, outFile) {
+  let formattedJsonStr = JSON.stringify(outStr, null, 2);
+
+  fs.writeFile(outFile, formattedJsonStr, function(err) {
+    if (err) {
+      return console.log("Error: " + err);
+    }
+  });
+}
+
+const req = https
+  .get(requestOpts, resp => {
+    let fullResponseJson = "";
+
+    resp.on("data", d => {
+      fullResponseJson += d;
+    });
+    resp.on("end", () => {
+      writeJsonToFile(filterJson(fullResponseJson), repoFile);
+    });
+  })
+  .on("error", err => {
+    console.log("Error: " + err.message);
+  });
+
+req.end();
