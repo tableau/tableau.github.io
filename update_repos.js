b6d7fd0217e4e375cd0f89da740c4ef7c32c2015
Stu Tomlinson <stu@nosnilmot.com>
2023-11-16 17:01:31 +0000

    Update repository list (#14)
    Update repos with latest list
    Sort saved list, so it doesn't need sorting when viewing

--- update_repos.js
+++ update_repos.js
@@ -40,6 +40,10 @@ function filterJson(reposJson) {
     repos.push(repoJsonSubset);
   });
 
+  repos.sort(function (a, b) {
+    return b.stargazers_count - a.stargazers_count;
+  });
+
   return repos;
 }
 
